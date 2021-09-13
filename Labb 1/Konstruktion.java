aaf0ea93e6e896074f1b2a51aa838785f61f0d83
Adeel Hussain <adeelh@kth.se>
2021-09-13 16:45:27 +0200

    Added buffered output
    Writes to file with BufferedWriter and BufferedOutputStream
    
    TODO
    Tweak P_Pos
    Write in bytes to Index P
    Add Index A and link with Index I
    
    Co-Authored-By: Rakin <54189419+RakinAli@users.noreply.github.com>

--- Labb 1/Konstruktion.java
+++ Labb 1/Konstruktion.java
@@ -1,15 +1,21 @@
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.File;
 import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.BufferedWriter;
+import java.io.BufferedOutputStream;
 import java.io.IOException; // Import the IOException class to handle errors
 import java.io.RandomAccessFile; // Import the RandomAccessFile
+
 /*
     Rakin Ali CINTE19, KTH
     Adeel Hussain CINTE19, KTH
     Påbörjad 2021-09-11
 */
 
-public class Konstruktion {
+public class Konstruktion 
+{
     /**
      * __________ psuedo koden_______________
      * 
@@ -38,11 +44,29 @@ public class Konstruktion {
     {
 
         // textReader läser filen
-        BufferedReader textReader = new BufferedReader(new FileReader("Nontxt"));
+        BufferedReader textReader = new BufferedReader(new FileReader("TestRawIndex.txt"));
 
         // These are the files where we create the I and P files.
-        RandomAccessFile iWriter = new RandomAccessFile("Test_RAI_Index", "rw");
+       // BufferedWriter 
+
+
+        // final byte [] isoByte = String.getByte("ISO-8859-1")
+
+        // isoByte.length
+
+        File i_index = new File("TEST_I_index");
+        File p_index = new File("TEST_P_index");
+
+        /* RADERA INTE -> VIKTIGA KOMMENTARER
+        RandomAccessFile iWriter = new RandomAccessFile("Test_RAF_I_Index", "rw");
         RandomAccessFile pWriter = new RandomAccessFile("Test_RAF_P_Index", "rw");
+        */
+
+        BufferedWriter iWriter = null;
+        FileWriter i_Writer = new FileWriter(i_index);
+        iWriter = new BufferedWriter(i_Writer);
+
+        BufferedOutputStream pWriter =  new BufferedOutputStream(new FileOutputStream(p_index));
 
         // Temp variable that stores the word previous and then compares it to see if
         // the word is unique or not.
@@ -51,20 +75,29 @@ public class Konstruktion {
         // Counts the frequencies of the word
         int wordfreq = 0;
 
+        // The byte position of P, where in P we are writing to 
+        long p_position = 0L;
+
+        // The byte position of I, where in I_index we are writing to 
+        long i_position = 0L;
+
         try 
         {
-            // String encoding = "ISO-8859-1";
-            String encoding = "UTF-8";
+            String encoding = "ISO-8859-1";
+            // String encoding = "UTF-8";
 
             // Space in byte
             String space = " ";
             byte[] spaceB = space.getBytes(encoding);
+            int space_toBytesLength = spaceB.length;
 
             // New line in bytes
             String newLine = "\n";
+            int newLineLength = newLine.getBytes().length;
             byte[] newLineB = newLine.getBytes(encoding);
             String data;
-            // While textDoc has a new word
+
+            // While textDoc has a new word and that word isn't null
             while ((data = textReader.readLine()) != null) 
             {
 
@@ -74,6 +107,7 @@ public class Konstruktion {
 
                 // The word from the text
                 index_Word = text_line[0];
+
                 // The byte index from the text
                 byteIndex = text_line[1];
 
@@ -84,43 +118,58 @@ public class Konstruktion {
                     if (!(word.equals(""))) 
                     {
                         // We add space
-                        iWriter.write(spaceB);
+                        iWriter.write(space);
+                        i_position += space_toBytesLength;
 
                         // Add frequency of the previous word (temp changed for readability)
                         String s_Wordfreq = "" + wordfreq;
-                        iWriter.writeBytes(s_Wordfreq);
+      
+                        //Writing to I index
+                        int s_WordfreqLength = s_Wordfreq.getBytes().length;
+                        i_position += s_WordfreqLength;
+                        iWriter.write(s_Wordfreq);
 
                         // Reset the wordfreq
                         wordfreq = 0;
 
                         // New line
-                        iWriter.write(newLineB);
+                        iWriter.write(newLine);
+                        i_position += newLineLength;
                     }
+
+                    // Encodes the ByteIndex from the text into a byte
+                    byte[] bytesByteIndex = byteIndex.getBytes(encoding);
 
                     // p_position is the byteIndex on file P, basically where we're currently
                     // writing to in file P.
-                    long p_position = pWriter.getFilePointer();
+                    int byteIndexLength = bytesByteIndex.length;
+                    //long p_position = pWriter.getFilePointer();
+                    p_position += byteIndexLength;
 
                     // Updates the word variable, it checks the word behind.
                     word = index_Word;
 
                     // Encodes the word from the text into a byte
-                    byte[] bytesWord = index_Word.getBytes(encoding);
+                   // byte[] bytesWord = index_Word.getBytes(encoding);
 
                     // Here we write the word to file I
-                    iWriter.write(bytesWord);
+                    i_position += index_Word.getBytes().length;
+                    iWriter.write(index_Word);
 
                     // We add space
-                    iWriter.write(spaceB);
+                    iWriter.write(space);
+                    i_position += space_toBytesLength;
 
                     // Now we add the position (temp changed for readability)
                     String sPos = "" + p_position;
-                    iWriter.writeBytes(sPos);
+                    i_position += sPos.getBytes().length;
+                    iWriter.write(sPos);
 
                     // Encodes the ByteIndex from the text into a byte
-                    byte[] bytesByteIndex = byteIndex.getBytes(encoding);
-
+                   // byte[] bytesByteIndex = byteIndex.getBytes(encoding);
+                    
                     // We write the ByteIndex in L to P
+                    p_position += bytesByteIndex.length;
                     pWriter.write(bytesByteIndex);
 
                     // Here we create a new line
@@ -135,10 +184,10 @@ public class Konstruktion {
                 else 
                 {
                     // Encodes the ByteIndex from the text into a byte
-                    byte[] bytesByteIndex = byteIndex.getBytes(encoding);
+                   // byte[] bytesByteIndex = byteIndex.getBytes(encoding);
 
                     // We write the ByteIndex in L to P
-                    pWriter.write(bytesByteIndex);
+                    pWriter.write(byteIndex.getBytes(encoding));
 
                     // Here we create a new line
                     pWriter.write(newLineB);
@@ -151,11 +200,13 @@ public class Konstruktion {
             if ((data = textReader.readLine()) == null) 
             {
                 // We add space
-                iWriter.write(spaceB);
+                iWriter.write(space);
+                i_position += space_toBytesLength;
 
                 // Add frequency of the previous word (temp changed for readability)
                 String s_Wordfreq = "" + wordfreq;
-                iWriter.writeBytes(s_Wordfreq);
+                i_position += s_Wordfreq.getBytes().length;
+                iWriter.write(s_Wordfreq);
             }
 
 
