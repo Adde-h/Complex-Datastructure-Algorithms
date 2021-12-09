eb3e9fc6e5f1a79c97d49f609654cc96499a614d
Adeel Hussain <adeelh@kth.se>
2021-12-09 20:02:41 +0100

    Lab 5 Done
    Co-Authored-By: Rakin <54189419+RakinAli@users.noreply.github.com>

---
+++ Labb 5/CastingHeuristic.java
@@ -0,0 +1,236 @@
+import java.util.ArrayList;
+
+public class CastingHeuristic {
+
+	static Kattio io = new Kattio(System.in, System.out);
+	static int roles, scenes, actors;
+
+	static ArrayList<Integer>[] rolesToActors; // Roles. and the actors that play them
+	static ArrayList<Integer>[] scenesToRoles; // Scenes.
+	static ArrayList<Integer>[] actorsArray; // Actors AND THEIR ROLES
+
+	@SuppressWarnings("unchecked")
+
+	/* Method that reads input */
+	static void readInput() {
+		int actorsToRead, rolesToRead, actor, role;
+
+		/* Get the in-data */
+		roles = io.getInt();
+		scenes = io.getInt();
+		actors = io.getInt();
+
+		/* Create array of Linked list */
+		rolesToActors = new ArrayList[roles + 1];				// +1 because of 0-indexing
+		scenesToRoles = new ArrayList[scenes + 1]; 			// +1 because of 0-indexing
+		actorsArray = new ArrayList[actors + roles + 1]; // +1 because of 0-indexing
+
+		/* Read the roles for each actor */
+		for (int i = 1; i <= roles; i++) {
+
+			// How many Actors playes this role?
+			actorsToRead = io.getInt();
+			rolesToActors[i] = new ArrayList<Integer>();
+
+			// Giving roll 1..n to all the actors
+			for (int j = 0; j < actorsToRead; j++) {
+				actor = io.getInt();
+				rolesToActors[i].add(actor);
+			}
+		}
+
+		/* Read the scenes for each role */
+		for (int i = 1; i <= scenes; i++) {
+			// How many roles in the scene
+			rolesToRead = io.getInt();
+			// Putting the roles to the scenes
+			scenesToRoles[i] = new ArrayList<Integer>();
+			for (int j = 0; j < rolesToRead; j++) {
+				role = io.getInt();
+				scenesToRoles[i].add(role);
+			}
+		}
+	}
+
+	/* Check if roles are not in the same scene, True if they are not in the same scene */
+	/* Help function ->This is where we find two roles to be the divas */
+	static boolean rolesSeparate(int role1, int role2) {
+		for (int i = 1; i < scenesToRoles.length; i++) {
+			// Does the scene have role 1 and role2? If yes then false cause diva
+			if ((scenesToRoles[i]).contains(role1) && (scenesToRoles[i]).contains(role2)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+/*
+  En skådespelare kan inte spela 2 olika roller i samma scen
+  
+
+ */
+	static boolean actorsSeperate(int actor, int role) {
+		if (actor == 1 || actor == 2) {
+			
+			/* Check for Diva 1 (actor 1) */
+			for (int i = 0; i < actorsArray[1].size(); i++) {
+				if (!rolesSeparate(actorsArray[1].get(i), role)) {
+					return false;
+				}
+			}
+			
+			/* Check for Diva 2 (actor 2) */
+			for (int i = 0; i < actorsArray[2].size(); i++) {
+				if (!rolesSeparate(actorsArray[2].get(i), role)) {
+					return false;
+				}
+			}
+		} 
+		else 
+		{
+			/* Check for chosen actor */
+			for (int i = 0; i < actorsArray[actor].size(); i++) {
+				if (!rolesSeparate(actorsArray[actor].get(i), role)) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	static void solver() 
+	{
+    /*
+    * Save all possible diva roles in a list then use this to find a solution
+    */
+		
+		ArrayList<Integer> diva1 = new ArrayList<Integer>();
+		ArrayList<Integer> diva2 = new ArrayList<Integer>();
+
+    /*
+      Puts linkedList, the linked list contains the roles the actor will play
+      in the solution
+    */
+		for (int i = 0; i < actorsArray.length; i++) {
+			actorsArray[i] = new ArrayList<Integer>();
+		}
+
+    /*
+    * First check all the roles the divas POSSIBLY PLAYS
+    * add them to the Diva linked list  
+    */
+		for (int i = 1; i < rolesToActors.length; i++) {
+			if ((rolesToActors[i]).contains(1)) {
+				diva1.add(i);
+			}
+			if ((rolesToActors[i]).contains(2)) {
+				diva2.add(i);
+			}
+		}
+
+		/**
+		 * Check if the divas are in the same scene
+		 * if they are not in the same scene then add them to the actors array
+		 * then clear the rolesToActors for the role when a role is taken
+		 */
+		for (int i = 0; i < diva1.size(); i++) {
+			for (int j = 0; j < diva2.size(); j++) {
+				if (rolesSeparate(diva1.get(i), diva2.get(j))) 
+				{
+          // This is a solution
+					actorsArray[1].add(diva1.get(i));
+					actorsArray[2].add(diva2.get(j));
+
+          // Clear the array so no other actor can play the same role
+					rolesToActors[diva1.get(i)].clear();
+					rolesToActors[diva2.get(j)].clear();
+					break;
+				}
+			}
+			/* Exit for-loop */
+			if (actorsArray[1].size() != 0) 
+			{
+				break;
+			}
+		}
+
+		/* Check all actors to find first best role solution, NOT DIVAS */
+		for (int i = 1; i < rolesToActors.length; i++) 
+		{
+      // if RolesToActor = NUll -> Diva stole the role 
+			for (int j = 0; j < rolesToActors[i].size(); j++) 
+			{
+				int currentActor = rolesToActors[i].get(j);
+
+				/* Get the first best solution */
+				if (actorsSeperate(currentActor, i)) 
+				{
+					actorsArray[currentActor].add(i);
+					rolesToActors[i].clear();
+				}
+			}
+		}
+
+		/* Assign super actors to leftout roles */
+		int superActor = actors + 1;
+		for (int i = 1; i <= roles; i++) 
+		{
+			if (rolesToActors[i].size() != 0) 
+			{
+				actorsArray[superActor++].add(i);
+			}
+		}
+	}
+
+	static void printSolve() {
+		// Counts how many actors got roles
+		int counter = 0;
+		for (int i = 1; i < actorsArray.length; i++) {
+			if ((actorsArray[i].size() != 0)) {
+				counter++;
+			}
+		}
+		io.println(counter);
+		for (int i = 1; i < actorsArray.length; i++) 
+		{
+			if (actorsArray[i].size() != 0) 
+			{
+				io.print(i + " " + actorsArray[i].size());
+				for (int j = 0; j < actorsArray[i].size(); j++) 
+				{
+					io.print(" " + actorsArray[i].get(j));
+				}
+				io.println();
+			}
+		}
+    io.flush();
+	}
+
+	/**
+	 * Indata
+	 * De tre första raderna består av antal roller, antal scener och antal skåde
+	 * n rader -> roll 1..n -> hur många skådepspelare sen skådesepelare
+	 * s rader -> Scener1...s -> antal roller sen alla roller som är med
+	 * 
+	 **/
+
+	/**
+	 * Utdata format:
+	 * Antalet skådespelare som fått roller
+	 * En rad för varje skådespelare (som fått roller) med skådespelarens nummer,
+	 * antalet roller skådespelaren tilldelats
+	 * samt numren för dessa roller
+	 * EX:
+	 * 1 (En skådespelare som fått roll)
+	 * 2 4 1 2 3 4 (Skådespelare 2, har fått 4 roller, roll 1,2,3,4)
+	 */
+
+	 
+
+	public static void main(String[] args) {
+		readInput();
+		solver();
+		printSolve();
+	}
+
+}
